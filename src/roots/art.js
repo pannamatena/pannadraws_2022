3c4443cfaf7a4ceac1f0a2657d1cc62c1ceb9e30
pannamatena <pannarajzol.design@gmail.com>
2022-02-03 18:51:57 +0000

    add more images

--- src/roots/art.js
+++ src/roots/art.js
@@ -1,12 +1,12 @@
 import MasonryLayout from '../components/MasonryLayout';
-import imgData from '../resources/pictures/2021/imgData_2021';
+import { imgData_2021 as imgDataAnimalArt_2021 } from '../resources/pictures/2021/imgData_animalArt_2021';
 
 export function AnimalArt() {
   return (
     <div>
       <h2>All Birds & Wildlife Art</h2>
 
-      <MasonryLayout imgData={imgData} />
+      <MasonryLayout imgData={imgDataAnimalArt_2021} />
       {/*<Link to="/irish_raptors/">Irish Raptors</Link>
       <Link to="/feathers_of_tales/">Feathers of Tales</Link>*/}
     </div>
